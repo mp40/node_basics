df8d8dda3c9c7e81a362a6b89187be166e0bc5f8
Matthew Philps <philps.matthew@gmail.com>
2019-11-26 08:45:22 +0900

    updated to basic express template

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
-const http = require('http');
-
 const express = require('express');
 
 const app = express();
 
-const server = http.createServer(app);
+app.use((req, res, next) => {
+    console.log('middleware');
+    next();
+});
 
-server.listen(3000);
+app.use((req, res, next) => {
+    console.log('next middleware')
+    res.send('<h1>Hi</h1>');
+});
+
+app.listen(3000);
