1480e6e4065c11abd6139af2dfa4a539db28e1fe
Matthew Philps <philps.matthew@gmail.com>
2019-11-28 08:23:41 +0900

    set up views

--- routes/admin.js
+++ routes/admin.js
@@ -1,12 +1,15 @@
+const path = require('path');
+
 const express = require('express');
 
 const router = express.Router();
 
+// /admin/add-product => GET
 router.get('/add-product', (req, res, next) => {
-    console.log('1, next middleware')
-    res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>');
+    res.sendFile(path.join(__dirname, '../', 'views', 'add-product.html'));
 });
 
+// /admin/add-product => POST
 router.post('/product', (req, res, next) => {
     console.log(req.body);
     res.redirect('/')
--- routes/shop.js
+++ routes/shop.js
@@ -1,10 +1,11 @@
+const path = require('path');
+
 const express = require('express');
 
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
-    console.log('2, next middleware')
-    res.send('<h1>Hi</h1>');
+    res.sendFile(path.join(__dirname, '../', 'views', 'shop.html'));
 });
 
 module.exports = router;
